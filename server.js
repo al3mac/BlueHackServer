15b5e2986c9d396a61aa1ddc87db77f211fec0e1
almac <al3mac@gmail.com>
2015-10-24 18:41:50 +0200

    working server ;)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var Hapi = require('hapi');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '192.168.1.108';
 
 var server = new Hapi.Server();
 
@@ -15,10 +15,48 @@ server.start(function () {
 });
 
 server.route({
-    method: 'GET',
-    path: '/api/hello',
+    method: 'POST',
+    path: '/api/getTest',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply({
+            userInput: request.payload.userInput
+            , result: 'ok!'
+        });
     }
-
 });
+
+server.route({
+    method: 'POST',
+    path: '/',
+    handler: function (request, reply) {
+        console.log(request.payload.author);
+        reply({
+                author: 'Audrey Hepburn'
+                , text: 'Nothing is impossible, the word itself says \'I\'m possible\'!'
+            }
+        );
+    }
+});
+
+
+server.route({
+    method: 'POST',
+    path: '/api/getMenu',
+    handler: getMenu
+});
+
+function getMenu(request, reply){
+
+    fs = require('fs')
+    fs.readFile(request.payload.restaurantName, 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(data);
+
+        reply(JSON.parse(data))
+    });
+
+
+
+}
